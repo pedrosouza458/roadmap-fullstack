c480ff8a0cde56a35e87ec6c45fb929212d044f0
Pedro Souza <pedro.ads510@gmail.com>
2025-11-05 21:41:44 -0300

    fix(user): add jpa mapping in user model

--- api/src/main/java/com/example/api/models/User.java
+++ api/src/main/java/com/example/api/models/User.java
@@ -1,6 +1,9 @@
 package com.example.api.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,6 +11,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -15,6 +19,7 @@ import lombok.Setter;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     private String name;
